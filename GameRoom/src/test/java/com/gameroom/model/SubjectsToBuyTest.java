c2ce7c9194bfe21b88ca6a9c33b57109593e6a43
kostya1205 <kostya.zhurovich12052@gmail.com>
2023-11-14 10:18:40 +0300

    Add test priceOfAll in time

--- GameRoom/src/test/java/com/gameroom/model/SubjectsToBuyTest.java
+++ GameRoom/src/test/java/com/gameroom/model/SubjectsToBuyTest.java
@@ -2,32 +2,34 @@ package com.gameroom.model;
 
 import com.gameroom.controller.GameMachine;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-public class SubjectsToBuyTest {
+class SubjectsToBuyTest {
+    private GameRoom room;
+    @BeforeEach
+    void setUp(){
+        room = new GameRoom();
+        GameMachine gameMachine = new GameMachine();
+        while(room.allSubjects().size()<20) gameMachine.buySubject(room);
+    }
     @Test
     public void sortByPrice(){
-        GameRoom room = new GameRoom();
-        GameMachine gameMachine = new GameMachine();
-        while(room.allSubjects().size()<10) gameMachine.buySubject(room);
+
         Comparator<SubjectToBuy> priceComparator = Comparator.comparingInt(SubjectToBuy::getPrice);
         List<SubjectToBuy> subjects = room.allSubjects().getAll();
         subjects.sort(priceComparator);
-        // Сортируем список с использованием компаратора
         room.sortByPrice();
-        for (int i = 0; i < subjects.size(); i++) {
+        for (int i = 0; i < subjects.size(); i++)
             Assertions.assertEquals(subjects.get(i).getPrice(),room.allSubjects().get(i).getPrice());
-        }
+
     }
     @Test
     public void sortByWeight(){
-        GameRoom room = new GameRoom();
-        GameMachine gameMachine = new GameMachine();
-        while(room.allSubjects().size()<10) gameMachine.buySubject(room);
         Comparator<SubjectToBuy> weightComparator = Comparator.comparingInt(SubjectToBuy::getWeight);
         List<SubjectToBuy> subjects = room.allSubjects().getAll();
         subjects.sort(weightComparator);
@@ -38,9 +40,6 @@ public class SubjectsToBuyTest {
     }
     @Test
     public void searchByName(){
-        GameRoom room = new GameRoom();
-        GameMachine gameMachine = new GameMachine();
-        while(room.allSubjects().size()<10) gameMachine.buySubject(room);
         for (SubjectToBuy subject : room.allSubjects().getAll()) {
             SubjectsToBuy subjectsToBuy2 = new SubjectsToBuy();
             List<SubjectToBuy> subjects = room.allSubjects().getAll();
@@ -51,9 +50,6 @@ public class SubjectsToBuyTest {
     }
     @Test
     public void searchByPrice(){
-        GameRoom room = new GameRoom();
-        GameMachine gameMachine = new GameMachine();
-        while(room.allSubjects().size()<10) gameMachine.buySubject(room);
         for (SubjectToBuy subject : room.allSubjects().getAll()) {
             SubjectsToBuy subjectsToBuy2 = new SubjectsToBuy();
             List<SubjectToBuy> subjects = room.allSubjects().getAll();
@@ -65,64 +61,53 @@ public class SubjectsToBuyTest {
 
     @Test
     public void priceOfAll(){
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < 100; i++) {
-
-            GameRoom room = new GameRoom();
-            GameMachine gameMachine = new GameMachine();
-            while (room.allSubjects().size() < 10) gameMachine.buySubject(room);
-
             int sum = 0;
             for (SubjectToBuy subjectToBuy : room.allSubjects().getAll()) {
                 sum += subjectToBuy.getPrice();
             }
             Assertions.assertEquals("Цена всех вещей равна " + sum, room.allSubjects().priceOfAll());
+            setUp();
         }
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        Assertions.assertFalse(executionTime > 50,"Время выполнения превысило 5 миллисекунд");
     }
     @Test
     public void weightOfAll(){
         for (int i = 0; i < 100; i++) {
-
-            GameRoom room = new GameRoom();
-            GameMachine gameMachine = new GameMachine();
-            while (room.allSubjects().size() < 10) gameMachine.buySubject(room);
-
             int sum = 0;
             for (SubjectToBuy subjectToBuy : room.allSubjects().getAll()) {
                 sum += subjectToBuy.getWeight();
             }
             Assertions.assertEquals("Вес всех Вещей равен " + sum, room.allSubjects().weightOfAll());
+            setUp();
         }
     }
     @Test
     public void maxWeight(){
         for (int i = 0; i < 100; i++) {
-
-            GameRoom room = new GameRoom();
-            GameMachine gameMachine = new GameMachine();
-            while (room.allSubjects().size() < 10) gameMachine.buySubject(room);
-
             SubjectToBuy maxSubjectToBuy = room.allSubjects().getAll().get(room.allSubjects().getAll().size() - 1);
             for (SubjectToBuy subjectToBuy : room.allSubjects().getAll()) {
                 if (maxSubjectToBuy.getWeight() < subjectToBuy.getWeight())
                     maxSubjectToBuy = subjectToBuy;
             }
             Assertions.assertEquals("Subject with max weight = " + maxSubjectToBuy, room.allSubjects().maxWeight());
+        setUp();
         }
     }
     @Test
     public void maxPrice(){
         for (int i = 0; i < 100; i++) {
-
-            GameRoom room = new GameRoom();
-            GameMachine gameMachine = new GameMachine();
-            while (room.allSubjects().size() < 10) gameMachine.buySubject(room);
-
             SubjectToBuy maxSubjectToBuy = room.allSubjects().getAll().get(room.allSubjects().getAll().size() - 1);
             for (SubjectToBuy subjectToBuy : room.allSubjects().getAll()) {
                 if (maxSubjectToBuy.getPrice() < subjectToBuy.getPrice())
                     maxSubjectToBuy = subjectToBuy;
             }
             Assertions.assertEquals("Subject with max price = " + maxSubjectToBuy, room.allSubjects().maxPrice());
+        setUp();
         }
     }
 }
